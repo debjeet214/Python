1d66343ed7f0bfb0d15f4fedd269b742ec07bbba
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-04-11 19:51:43 +0530

    Create endswith function.py

---
+++ Collab study/string methods/endswith function.py
@@ -0,0 +1,9 @@
+# Returns true if the string ends with the specified value
+
+txt = "debjeetghosh@gmail.com"
+
+x = txt.endswith(".com")
+
+print(x)
+
+# it can be useful in the case where we have to identify if the following gmail is correct  or not ? 
