2de5f5bd146c81369b07fa816fde5733e7581d3e
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-04-11 14:34:28 +0530

    Update 1.2 change input value with int function.py

--- Collab study/1.2 change input value with int function.py
+++ Collab study/1.2 change input value with int function.py
@@ -2,3 +2,9 @@ birth_year = input("enter your birth year : ")
 age = 2024 - int(birth_year)
 print("Your current age is ")
 print(age)
+
+
+# some pther buil;d-in functions to convert the type of inputs are: 
+# 1. float()
+# 2. str()
+# 3. bool()
