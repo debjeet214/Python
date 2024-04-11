5b391da89466fb96bf34167215f62aff1ddcc884
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-04-11 18:59:46 +0530

    Create Basic String methods.py

---
+++ Collab study/string methods/Basic String methods.py
@@ -0,0 +1,20 @@
+# explaining the sting methods
+
+myname = 'my name is DEbjeeT GhosH. you know me?'
+print("Actual line is = " + myname)
+
+print("1. Upper Function" + myname.upper())    # make a copy of the actual string and change the case of the letters into uppercase
+
+print("2. lower functions")
+print(myname.lower())    # make a copy of the actual string and change the case of the letters into lowercase
+print(myname.casefold())
+
+find_char = myname.find('is')
+print( "3. Find a specific word index = " + str(find_char))   # this will return the index position of the particular  letter in the string if it exists, otherwise returns null
+
+print("4. Make the first character capital = " + myname.capitalize()) # this will make the first charater upper case.
+
+print(myname.split('is', 5))   # split the string at the first occurrence of
+
+print(myname.count('me'))  # count the no of times a value occur in the string
+print(myname)            # still the actual string stays as the same.
