ec4bead30f9f90b9d42a85295957517ac78857d8
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-04-16 22:15:57 +0530

    Create 1.2 find, split, count method.py

---
+++ Collab study/string methods/1.2 find, split, count method.py
@@ -0,0 +1,8 @@
+myname = 'my name is DEbjeeT GhosH. you know me?'
+
+find_char = myname.find('is')
+print( "3. Find a specific word index = " + str(find_char))   # this will return the index position of the particular  letter in the string if it exists, otherwise returns null
+
+print(myname.split('is', 5))   # split the string at the first occurrence of
+
+print(myname.count('me'))  # count the no of times a value occur in the string
