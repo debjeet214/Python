41cf4d4ec635aaf2578cd9465acceea29d5ab8da
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-04-16 22:53:42 +0530

    Update and rename 4.2 Replace method.py to 4.2 Replace & in method.py

---
+++ Collab study/string methods/4.2 Replace & in method.py
@@ -0,0 +1,8 @@
+#  this helps to replace a subset with another value.
+
+str = ' Debjeet has love for all people'
+print(str.replace('for', '4'))
+
+#  this will return true only if there is the sub value present in the actual string
+
+print('love' in str)  # o/p is true here
--- Collab study/string methods/4.2 Replace method.py
+++
@@ -1,4 +0,0 @@
-#  this helps to replace a subset with another value.
-
-str = ' Debjeet has love for all people'
-print(str.replace('for', '4'))
