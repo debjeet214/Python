84bb4f64fd59c7ad0b58559cbf065e459cfe8d85
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-04-16 22:36:10 +0530

    Update 3. check methods.py

--- Collab study/string methods/3. check methods.py
+++ Collab study/string methods/3. check methods.py
@@ -10,3 +10,16 @@ isprintable()	Returns True if all characters in the string are printable
 isspace()	Returns True if all characters in the string are whitespaces
 istitle()	Returns True if the string follows the rules of a title
 isupper()	Returns True if all characters in the string are upper case
+
+### some common example
+
+stringing = 'The king of the ALberta is JACK 2 ? is not it'
+print(stringing.isalnum()) # o/p is false as not all the characters in the string is number
+print(stringing.isalpha())  # checks if all the characters are alphabetrs or not
+print(stringing.isascii())   # checks if all the strings are ascii values
+print(stringing.islower())   # checks if all the string characters are in lower case or not
+
+string_val = " HI THERE ARE YOU OK "
+print(string_val.isupper())  # checks if all the string characters are in upper case
+
+
