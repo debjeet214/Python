6d55e9c45df3af999864536648473ff1ffa4961e
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-04-16 22:14:38 +0530

    Update and rename 1.1 Basic String methods.py to 1.1 Basic case conversion.py

--- Collab study/string methods/1.1 Basic case conversion.py
+++ Collab study/string methods/1.1 Basic case conversion.py
@@ -12,12 +12,5 @@ print(myname.casefold())
 print(myname.swapcase())    # this w3ill swap all the cases to other means, lower to upper an dupper to lower
 print(myname.title())    # this will convert the string into a title like form
 
-find_char = myname.find('is')
-print( "3. Find a specific word index = " + str(find_char))   # this will return the index position of the particular  letter in the string if it exists, otherwise returns null
 
-print("4. Make the first character capital = " + myname.capitalize()) # this will make the first charater upper case.
-
-print(myname.split('is', 5))   # split the string at the first occurrence of
-
-print(myname.count('me'))  # count the no of times a value occur in the string
-print(myname)            # still the actual string stays as the same.
+print(myname)            #no matter what method is applied, still the actual string stays as the same. as only copies are made and used to update
