0bbf0275f51c971ae99d6e3b9b42988eae7ac490
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-04-20 00:16:14 +0530

    Create normal data.py

---
+++ Learning/data types/normal data.py
@@ -0,0 +1,15 @@
+a = 1
+b = True
+c = 'Harry'
+d = None
+f = a
+com = complex(9, 21)    # complex data type
+print(com)
+print("type of d is ", type(com))   # using type we can find the type of the variable.
+print("type of d is ", type(f))
+f = 1.652
+print(a+f)
+print(d)
+print("type of d is ", type(b))
+print("type of d is ", type(f))
+print("type of d is ", type(d))
