6e1ff02711645ebd60eee86b12c5f2314f5442df
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-04-21 00:32:51 +0530

    Create Basic methods.py

---
+++ Learning/day-2.1 List/Basic methods.py
@@ -0,0 +1,22 @@
+list_val = [1, 65, -3, 0, 76, 21, 8, 1]
+
+list_val.append(7)   # this method add a value at the end
+list_val.insert(0, 87)  # to add a value at any index position of the list
+list_val.remove(7)   # to remove a value from the list
+
+print(list_val.index(0))    # this is used to retun the index no. of the given value
+print(list_val.count(1))    # this will return the no. of times a value repeats in the list
+
+print(22 in list_val)  # to find a item in the list which will return true or false
+
+print(len(list_val))  # check how many elements are in the list.
+
+list_val.sort()   # sort the array in ascending order
+print(list_val)
+
+list_val.sort(reverse = True)   # sort the array in descending order by reversing it
+print(list_val)
+
+list_val.reverse()   # reverse the order of the list elements
+print(list_val)
+
