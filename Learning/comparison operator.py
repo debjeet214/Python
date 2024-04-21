87e40a4b9aa10c37a7324acc5242de2dea4e3e7a
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-04-21 21:57:17 +0530

    Create comparison operator.py

---
+++ Learning/comparison operator.py
@@ -0,0 +1,14 @@
+num1 = int(input("enter the first number : "))
+num2 = int(input("enter the second number: "))
+
+print(num1<num2)
+print(num1>num2)
+print(num1<=num2)
+print(num1>=num2)
+print(num1 == num2)
+print(num1 != num2)
+
+if( num1>0 and num2>0 ) :
+  print("Both are the valid number ")
+else:
+  print("This is not valid, enter a valid one")
