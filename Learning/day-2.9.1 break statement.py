ed2977186bd78668ef07968283054d2df17bcfae
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-04-23 21:10:25 +0530

    Create day-2.9.1 break statement.py

---
+++ Learning/day-2.9.1 break statement.py
@@ -0,0 +1,6 @@
+for i in range(1, 13):
+  print("5 X", i, "=", 5*i )    # here until 10 it will going on
+  if(i==10):
+    break
+
+# here until the value of i is equal to 10, the multiplication table will keep going 
