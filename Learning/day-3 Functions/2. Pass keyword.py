6fd2a2216a4129647fa66978b00b3294d41b80b3
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-04-24 00:40:09 +0530

    Create 2. Pass keyword.py

---
+++ Learning/day-3 Functions/2. Pass keyword.py
@@ -0,0 +1,6 @@
+#Pass keyword - this particular keyword is used as the body of the function that tells that, just skip the incomplete function and leave it for later and execute the next codes.
+
+def func(a, b):
+  pass 
+
+# using the 'PASS' will not execute error compiler triggers incomplete function 
