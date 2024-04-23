bdc7440ddf0d3c1dac68af15877ef17121aa19b5
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-04-24 01:15:52 +0530

    Create 5. Required arguments.py

---
+++ Learning/day-3 Functions/5. Required arguments.py
@@ -0,0 +1,6 @@
+# in scenario like where no value is passed then  we have to add value while function call this type of argument is called required argument
+
+def arg(x, y = 32): # here x is required argument only
+  return x+y
+
+arg(44)
