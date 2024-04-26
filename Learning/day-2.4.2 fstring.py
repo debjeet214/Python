65e2b279cdeb82e8a24423f904c1fdb046989967
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-04-27 02:05:17 +0530

    Update day-2.4.2 fstring.py

--- Learning/day-2.4.2 fstring.py
+++ Learning/day-2.4.2 fstring.py
@@ -1,3 +1,7 @@
+# It is a new string formatting mechanism introduced by the PEP 498. It is also known as Literal String Interpolation or more commonly as F-strings (f character preceding the string literal). The primary focus of this mechanism is to make the interpolation easier.
+
+#When we prefix the string with the letter 'f', the string becomes the f-string itself. The f-string can be formatted in much same as the str.format() method. The f-string offers a convenient way to embed Python expression inside string literals for formatting.
+
 name = input("Enter Your name: ")
 age = int(input("Enter your age: "))
 
