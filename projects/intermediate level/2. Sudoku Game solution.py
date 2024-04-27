d1a5ee268f2d37d9ed43316865f93973f0a6fd3e
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-04-28 00:43:08 +0530

    Create  2. Sudoku Game solution.py

---
+++ projects/intermediate level/2. Sudoku Game solution.py
@@ -0,0 +1,83 @@
+# using the backtracking algorithm
+
+board = [
+    [7,8,0,4,0,0,1,2,0],
+    [6,0,0,0,7,5,0,0,9],
+    [0,0,0,6,0,1,0,7,8],
+    [0,0,7,0,4,0,2,6,0],
+    [0,0,1,0,5,0,9,3,0],
+    [9,0,4,0,6,0,0,0,5],
+    [0,7,0,3,0,0,0,1,2],
+    [1,2,0,0,0,7,4,0,0],
+    [0,4,9,2,0,6,0,0,7]
+]
+
+def valid(bo_val, num, pos):
+    # check row
+    for i in range(len(bo_val[0])):
+        if bo_val[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # check column
+    for i in range(len(bo_val)):
+        if bo_val[i][pos[1]] == num and pos[0] != i:
+            return False
+        
+    # check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range (box_y*3, box_y*3 + 3):
+        for j in range(box_x*3, box_x*3 + 3):
+            if bo_val[i][j] == num and (i,j) != pos:
+                return False
+    return True
+
+
+def print_board(bo_val):
+
+    for i in range(len(bo_val)):
+        if i%3 == 0 and i != 0:
+            print("----------------------")
+        for j in range(len(bo_val[0])):
+            if j%3 == 0 and j != 0:
+                print(" | ", end="")
+            if j == 8:
+                print(bo_val[i][j])
+            else:
+                print(str(bo_val[i][j]) + " ", end="")
+
+
+def find_empty(bo_val):
+    for i in range(len(bo_val)):
+        for j in range(len(bo_val[0])): 
+            if bo_val[i][j] == 0:
+                return (i, j)
+    return None
+
+
+def solve(bo_val):
+    find = find_empty(bo_val)
+    if not find:
+        return True
+    
+    else:
+        row, col = find
+
+    for i in range(1, 10):
+        if valid(bo_val, i, (row, col)):
+            bo_val[row][col] = i
+
+            if solve(bo_val):
+                return True
+
+            bo_val[row][col] = 0
+
+    return False
+
+
+print_board(board)
+solve(board)
+print("----------------------------------")
+print("----------------------------------")
+print_board(board)
