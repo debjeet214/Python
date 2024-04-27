7be5dd4cef05e36b230318a46a55fd786609d892
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-04-27 11:39:46 +0530

    Create Series by Iterative approach.py

---
+++ competitive code/Fibonacci series/Series by Iterative approach.py
@@ -0,0 +1,15 @@
+
+# Python program to print first n Fibonacci numbers
+
+def printFibonacciNumbers(n):
+    f1 = 0
+    f2 = 1
+    if (n < 1):
+        return
+    print(f1, end=" ")
+    for x in range(1, n):
+        print(f2, end=" ")
+        next = f1 + f2
+        f1 = f2
+        f2 = next
+printFibonacciNumbers(7)
