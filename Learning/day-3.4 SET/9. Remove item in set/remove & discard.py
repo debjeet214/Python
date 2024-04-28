24f25b99ba7a993d2cd8736638cee631138efd64
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-04-28 14:55:25 +0530

    Create remove & discard.py

---
+++ Learning/day-3.4 SET/9. Remove item in set/remove & discard.py
@@ -0,0 +1,8 @@
+# remove and discard function.  to remove a single item from set.
+cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+cities.remove("Tokyo")
+print(cities)
+cities.remove("Moscow")    # this will return error 
+cities.descard("Moscow")   # now it will not return error
+
+
