1917b208fc36033e36ca9ad3ac7c197cc8d758ca
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-04-28 14:56:35 +0530

    Create pop.py

---
+++ Learning/day-3.4 SET/9. Remove item in set/pop.py
@@ -0,0 +1,6 @@
+cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+item = cities.pop()
+print(cities)
+print(item)
+
+# using pop it will remove the last element from the set. and as set is unsorted, so noones knows which item is going to be popped
