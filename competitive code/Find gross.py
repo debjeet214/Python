71918350509643cdc2be42915261f8c4633f42ca
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-05-18 18:25:47 +0530

    Update Find gross.py

--- competitive code/Find gross.py
+++ competitive code/Find gross.py
@@ -7,5 +7,19 @@ if hours <= 40:
     gross_pay = hours * rate
 else:
     gross_pay = 40 * rate + (hours - 40) * rate * 1.5
-    
 print("Gross Pay will be :", gross_pay)
+
+
+# doing the same using the function
+
+def computepay(hours, rate):
+    if hours <= 40:
+        pay = hours * rate
+    else:
+        pay = 40 * rate + (hours - 40) * rate * 1.5
+    return pay
+hours = float(input("Enter hours worked: "))
+rate = float(input("Enter hourly rate: "))
+
+gross_pay = computepay(hours, rate)
+print("Pay", gross_pay)
