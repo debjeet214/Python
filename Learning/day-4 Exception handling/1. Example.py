acca44ca32c91771b8e2d58aaa9e3246da7ac102
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-05-27 08:48:53 +0530

    Update and rename 1. Try & Except Block.py to 1. Example.py

--- Learning/day-4 Exception handling/1. Example.py
+++ Learning/day-4 Exception handling/1. Example.py
@@ -1,7 +1,3 @@
-# try block is used to ented a piece of code to check if it is ok or not.
-# if the try block is satisfied then the except block is skipped but
-# if the try block is false then it will not show error but it will enter the except block and execute the code in it.
-
 marks = input("enter your mnarks: ")
 
 try:
