9a0ebe10808e551b1496f944eefa452b6c07cd45
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-07-08 09:33:17 +0530

    Update day-1.6 Doc String.py

--- Learning/day-1.6 Doc String.py
+++ Learning/day-1.6 Doc String.py
@@ -3,7 +3,6 @@
 # Python Comments ::  Comments are descriptions that help programmers better understand the intent and functionality of the program. They are completely ignored by the Python interpreter.
 
 # Python docstrings ::  As mentioned above, Python docstrings are strings used right after the definition of a function, method, class, or module (like in Example 1). They are used to document our code.
-
 # We can access these docstrings using the doc attribute.
 
 def square(n):
