44574d4e0d9c4a81706fa84753fda8fc93aa9214
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-07-08 22:20:02 +0530

    Create 4. filter & reduce.py

---
+++ Learning/* highlight me/4. filter & reduce.py
@@ -0,0 +1,11 @@
+# filter = The filter function filters a sequence of elements based on a given predicate (a function that returns a boolean value) and returns a new sequence containing only the elements that meet the predicate. Means, if we apply a specific filter on a list elements then, it will filter out them by returning true or false for each elements. and only return the true elements in the result.
+
+def filter_function(a):
+  if a >10:
+    return a
+  else:
+    return 0
+  
+num = [ 3, 6, 9, 10, 13, 21, 3, 51, 8, 14, 27]
+newlist = list(filter(filter_function, num))   # applying the function on each elements to filter
+print(newlist)
