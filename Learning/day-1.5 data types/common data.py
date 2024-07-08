8d05d613bf552a3d1e3dae06a31160f7fff2e24f
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-07-08 09:40:07 +0530

    Update common data.py

--- Learning/day-1.5 data types/common data.py
+++ Learning/day-1.5 data types/common data.py
@@ -5,11 +5,13 @@ d = None
 f = a
 com = complex(9, 21)    # complex data type
 print(com)
-print("type of d is ", type(com))   # using type we can find the type of the variable.
-print("type of d is ", type(f))
+print("type of com is ", type(com))   # using type we can find the type of the variable.
+print("type of f is ", type(f))
 f = 1.652
 print(a+f)
 print(d)
-print("type of d is ", type(b))
-print("type of d is ", type(f))
+print("type of a is ", type(a))
+print("type of b is ", type(b))
+print("type of f is ", type(f))
 print("type of d is ", type(d))
+print("type of c is ", type(c))
