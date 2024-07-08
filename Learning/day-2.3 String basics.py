496c4ed36e658a94d3158b9853c619eeb664daf4
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-07-08 10:42:28 +0530

    Update day-2.3 String basics.py

--- Learning/day-2.3 String basics.py
+++ Learning/day-2.3 String basics.py
@@ -12,10 +12,10 @@ for character in name:
 fruit = "Mango"
 mangoLen = len(fruit)
 print(mangoLen)
-print(fruit[0:4]) # including 0 but not 4
-print(fruit[1:4]) # including 1 but not 4
-print(fruit[:5])      # automatically add 0 at the start
-print(fruit[0:-3])    # negative indexing 
-print(fruit[:len(fruit)-3])
-print(fruit[-1:len(fruit) - 3])
+print(fruit[0:4])               # including 0 but not 4
+print(fruit[1:4])               # including 1 but not 4
+print(fruit[:5])                # automatically add 0 at the start
+print(fruit[0:-3])              # negative indexing 
+print(fruit[:len(fruit)-3])     # same as before one
+print(fruit[-1:len(fruit) - 3]) # it will return nothing
 print(fruit[-3:-1])
