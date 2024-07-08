ccdba422ee00987959743e40d68be701498bba78
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-07-08 19:38:14 +0530

    Update day-2.8.1 If-else in single line.py

--- Learning/day-2.8.1 If-else in single line.py
+++ Learning/day-2.8.1 If-else in single line.py
@@ -1,3 +1,10 @@
 a = 10
 b = 10
 print("A") if a>b else print("Both equal") if a == b else print("B")
+
+
+# application
+age = int(input("Enter your age: "))
+print(age)
+
+print("You are Eligible to vote")if age>18 else print("Enter your month to know more") if age == 18 else print("You are not eligible to vote.")
