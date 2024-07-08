370e9a4e709a4463a2b9e6d574cc7b6e7fd68d17
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-07-08 10:20:27 +0530

    Update Concatenation.py

--- Learning/day-2.1 List/Concatenation.py
+++ Learning/day-2.1 List/Concatenation.py
@@ -8,7 +8,12 @@ list2 = [1, 30, 9, 7]
 list3 = list1 + list2
 print(list3)
 
+list3.sort()
+print(list3)
+
 # way 2
 
 list1.extend(list2)   # this will extend the list2 inside list1 making it lenghtly
-print(list1)
+print(list1)  # this will not require to make a seperate list as it just add all list2 elements into the list 1
+
+print(list2)
