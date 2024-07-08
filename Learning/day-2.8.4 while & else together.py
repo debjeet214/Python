375b0e03bf5450ecf20499558da38e530cf1cec5
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-07-08 18:00:13 +0530

    Update day-2.8.4 while & else together.py

--- Learning/day-2.8.4 while & else together.py
+++ Learning/day-2.8.4 while & else together.py
@@ -4,3 +4,13 @@ while (count > 0):
   count = count - 1
 else:
   print("I am done with while part so now i'm inside else")
+
+
+# funny example
+total = 10
+while(total > 0 ):
+  i = total
+  print(f"You slapped for the {i} times now")
+  total -= 1
+else:
+  print("You are beating me more than 10 times, im leaving home now")
