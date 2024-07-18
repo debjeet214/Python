dfb8dd081c4bd157a48ae15df16a2717b6f9ff05
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-07-18 17:11:21 +0530

    Create 3. exp - decorators.py

---
+++ Advance Functions/9. Decorators/3. exp - decorators.py
@@ -0,0 +1,17 @@
+# converting the 2 num addition to 3 num addition
+
+def decorator(func):
+  def inner():
+    result = func()
+    num3 = float(input("Enter third number: "))
+    result = result + num3
+    return result
+  return inner         # without changing the addition() function, we change the functionality
+
+def addition():        # function to add 2 values
+  num1 = float(input("Enter first number: "))
+  num2 = float(input("Enter second number: "))
+  return num1 + num2
+
+sum = decorator(addition)
+print(sum())
