434bdd47470da89298fe86a3240242da93397480
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-08-02 01:26:35 +0530

    Create lambda functions.py

---
+++ Advance Functions/lambda functions.py
@@ -0,0 +1,12 @@
+# A lambda function is a small anonymous function defined with the lambda keyword in programming languages like Python. It is used primarily for creating small, one-off functions without needing to formally define a function using the def keyword.
+
+#ex-1
+# lambda function
+
+def app(fx, value):
+  print(fx(value))
+
+# to create a lambda function by taking function as variable
+double = lambda x: x*2
+print(double(10))
+app(lambda x: x*x, 10)
