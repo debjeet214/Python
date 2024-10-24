f23a8536b91b0f9ff4d095961ada67988879a313
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-10-24 19:39:44 +0530

    Update 2. Search function.py

--- Learning/day-9 Regular Expression/2. Search function.py
+++ Learning/day-9 Regular Expression/2. Search function.py
@@ -1,8 +1,8 @@
-# The search() function searches the string for a match, and returns a Match object if there is a match.
+# The search() function searches the string for a match and returns a Match object if there is a match.
 
 import re
 txt = "The rain in Spain"
-x = re.search("\s", txt)  # this means it will search for the first white-space in the string 
+x = re.search("\s", txt)  # This means it will search for the first white space in the string 
 print("The first white-space character is located in position:", x.start()) # This will return the starting index value of the string
 
 # example 2
@@ -10,9 +10,17 @@ print("The first white-space character is located in position:", x.start()) # Th
 import re
 txt = "The rain in Spain"
 x = re.search("Spain", txt)
-print(x)                     # this will return the actual match object and description 
-print(x.start())             # this will return the starting index of the search thing.
+print(x)                     # This will return the actual match object and description 
+print(x.start())             # This will return the starting index of the search thing.
 
 # o/p
 # <re.Match object; span=(12, 17), match='Spain'>
 # 12
+-----------
+# example 3
+import re
+
+txt = "The rain in Spain"
+x = re.search("Portugal", txt)
+print(x)
+# it will return none if there is no match found in the string.
